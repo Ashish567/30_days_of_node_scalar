2f2f7907a4aa285db98328ca78929a1067f0c22f
Ashish Kumar Singh <djdaydreamer.singh4@gmail.com>
2024-02-03 00:01:52 +0530

    Day 2 revising writting to a file and appending 2 a file done

--- src/30_days/Day_1.ts
+++ src/30_days/Day_1.ts
@@ -1,5 +1,5 @@
 import chalk from "chalk";
-import { FileHandle, readFile } from "fs/promises";
+import { readFile, writeFile, appendFile } from "fs/promises";
 import path from "path";
 class Day_1 {
   constructor() {
@@ -10,11 +10,35 @@ class Day_1 {
     );
     console.log(chalk.blue("Hello") + " Day " + chalk.red("1 !"));
   }
-
+  // Day 1
   async read_file_async(fs_path: string) {
     try {
       const file_path = path.resolve(fs_path);
       const fileContent = await readFile(file_path, "utf-8");
+      console.log(chalk.whiteBright(fileContent));
+    } catch (error: any) {
+      console.error(
+        chalk.bgRedBright(`Error reading the file: ${error.message}`)
+      );
+    }
+  }
+  // Day 2
+  async write_file_async(fs_path: string, content: string) {
+    try {
+      const file_path = path.resolve(fs_path);
+      const fileContent = await writeFile(file_path, content);
+      console.log(chalk.whiteBright(fileContent));
+    } catch (error: any) {
+      console.error(
+        chalk.bgRedBright(`Error reading the file: ${error.message}`)
+      );
+    }
+  }
+  // Day 2
+  async append_file_async(fs_path: string, content: string) {
+    try {
+      const file_path = path.resolve(fs_path);
+      const fileContent = await appendFile(file_path, content);
       console.log(chalk.whiteBright(fileContent));
     } catch (error: any) {
       console.error(
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { Day_1 } from "./30_days";
+import { createReadStream } from "fs";
+import path from "path";
 
 dotenv.config();
 
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 
 const day = new Day_1();
 day.read_file_async(__dirname + "/../files/test.text");
+day.write_file_async(__dirname + "/../files/output.text", "\nHi from file....");
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
